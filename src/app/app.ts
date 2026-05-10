1ebfa17c5d458986db37ce0ebd181496b947ddb9
adamalfarizi <adam.alfarizi.2002@gmail.com>
2026-05-10 13:58:15 +0700

    feat: adding for side menu navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: './app.component.html',
 })
 export class App {
   protected readonly title = signal('nuxatech-project');
